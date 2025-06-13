4e7147274507fa94764346fbd985ca829a2bcddb
meatwad787 <greg_mrjohnson@yahoo.com>
2025-06-12 22:22:19 -0400

    Add a factory function to make 2D array, add gamecontroller methods

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // An IIFE to house the entire Game
 const TicTacToe = (function() {
-  const Game = {
-    gameboard : [],
+const Game = {
+    gameboard: [],
+// A factory function that loops and 
+// somehow makes 3 arrays inside gameboard
+    MakeArray: function(row, col, init) {
+      const arr = [];
+      for (let i = 0; i < row; i++) {
+        arr[i] = [];
+        for (let j = 0; j < col; j++) {
+          arr[i][j] = init;
+        }
+      }
+      this.gameboard = arr; // store it inside Game object
+      return arr;
+    }
   };
+
+  Game.MakeArray(3, 3, 0);
+  console.log(Game.gameboard);
 
 // A factory function to create new players
   function CreatePlayers(name, marker) {
@@ -20,12 +36,24 @@ const computer = CreatePlayers('CPU1', 'O');
 
   const GameController = {
     //Some methods to place player's markers
+     // Public method to set a cell on the gameboard
+      setCell: function(row, col, marker) {
+        Game.gameboard[row][col] = marker;
+      },
+      // Public method to get a cell value
+      getCell: function(row, col) {
+        return Game.gameboard[row][col];
+      },
+      // Public method to get the entire gameboard (for displaying)
+      getGameboard: function() {
+        return Game.gameboard;
+      }
   }
 
 // Anything in this return becomes
 // accessable outside the IIFE (public)
   return {
-    
+    Game,
     player1, 
     computer,
     GameController
